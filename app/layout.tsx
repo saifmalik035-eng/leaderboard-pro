7e9dfd8d88b7fe6f145e0d02d58610c1915673a0
Saif Malik <saifmalik035@gmail.com>
2026-04-03 09:42:58 +0530

    fix layout error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Leaderboard",
-};
-
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
